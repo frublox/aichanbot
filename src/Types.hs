5ff2efe3277f222e2c6df638dc57a6677a163de4
frublox <lainislaw@gmail.com>
2017-02-19 21:01:40 -0500

    Actually add the Types module (lol)

---
+++ src/Types.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Types where
+
+import Data.Text (Text)
+
+import Control.Lens
+
+import Text.Parsec
+import Text.Parsec.Text
+import Network.IRC.Client
+
+makeLenses ''InstanceConfig
+makeLenses ''Event
+
+data BotConfig = BotConfig
+    { _botNick :: Text
+    , _pass :: Text
+    , _channel :: Text
+    }
+makeLenses ''BotConfig
+
+data BotState = BotState
+    { _config :: BotConfig
+    }
+makeLenses ''BotState
+
+initBotState :: BotConfig -> BotState
+initBotState conf = BotState
+    { _config = conf
+    }
+
+data Command
+    = CmdUnknown
+    | CmdCommands
+    | CmdHi
+
+command :: Parser Command
+command = do
+    char '!'
+    cmd <- many alphaNum
+
+    case cmd of
+        "commands" -> return CmdCommands
+        "list" -> return CmdCommands
+        "hi" -> return CmdHi
+        _ -> return CmdUnknown
