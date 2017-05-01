5300c31e24cd749b561af4185396361a35b358f6
frublox <lainislaw@gmail.com>
2017-04-30 20:41:07 -0400

    Begin overhaul

--- src/Bot.hs
+++ src/Bot.hs
@@ -1,52 +1,69 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE TemplateHaskell            #-}
 
 module Bot where
 
 import           Data.Aeson
-import qualified Data.ByteString.Lazy   as Bytes
+import           Data.ByteString        (ByteString)
+import qualified Data.ByteString.Lazy   as BytesL
+import           Data.Ini
 import           Data.Map.Strict        (Map)
 import qualified Data.Map.Strict        as Map
 import           Data.Text              (Text)
-import qualified Data.Text              as Text
 
 import           Control.Concurrent.STM
 import           Control.Lens
 import           Control.Monad.IO.Class (MonadIO, liftIO)
 import           Control.Monad.Reader
-import           Network.IRC.Client
+import           Control.Monad.State
 
 import           System.Exit            (die)
 
 import           Command                (CommandInfo)
 import           Lifted                 (atomicallyL)
 
+import           Irc                    (runIrcBot)
+
 data BotConfig = BotConfig
-    { _botNick :: Text
-    , _pass    :: Text
-    , _channel :: Text
+    { _botNick    :: Text
+    , _botPass    :: Text
+    , _channel    :: Text
+    , _staticCmds :: Map Text CommandInfo
+    , _inputChan  :: TChan Text
+    , _outputChan :: TChan Text
     }
 makeLenses ''BotConfig
 
-data BotState = BotState
-    { _config      :: BotConfig
-    , _staticCmds  :: Map Text CommandInfo
-    , _dynamicCmds :: TVar (Map Text Text)
+initBotConfig :: MonadIO io => Ini -> TChan Text -> TChan Text -> io (Either String BotConfig)
+initBotConfig ini input output = do
+    staticCmds' <- liftIO $ do
+        bytes <- BytesL.readFile "commands.json"
+        either die return (eitherDecode bytes)
+
+    BotConfig
+        <$> lookupValue "config" "nick" ini
+        <*> lookupValue "config" "pass" ini
+        <*> lookupValue "config" "channel" ini
+        <*> staticCmds'
+        <*> input
+        <*> output
+
+newtype BotState = BotState
+    { _dynamicCmds :: Map Text Text
     }
 makeLenses ''BotState
 
-initBotState :: MonadIO io => BotConfig -> io BotState
-initBotState botConf = do
-    staticCmds' <- liftIO $ do
-        bytes <- Bytes.readFile "commands.json"
-        either die return (eitherDecode bytes)
+initBotState :: BotState
+initBotState = BotState
+    { _dynamicCmds = Map.empty
+    }
 
-    dynamicCmds' <- atomicallyL (newTVar Map.empty)
+newtype Bot a = Bot { unBot :: MonadIO io => ReaderT BotConfig (StateT BotState io) a }
+    deriving (Functor, Applicative, Monad, MonadIO, MonadState BotState, MonadReader BotConfig)
 
-    return BotState
-        { _config = botConf
-        , _staticCmds = staticCmds'
-        , _dynamicCmds = dynamicCmds'
-        }
+runBot :: MonadIO io => Bot a -> BotConfig -> BotState -> io a
+runBot bot botConf botState = do
+    let unReader = runReaderT (unBot bot) botConf
+    runStateT unReader botState
 
-type Bot = ReaderT (IRCState BotState) IO
--- src/Main.hs
+++ src/Main.hs
@@ -8,10 +8,12 @@ import qualified Data.Map.Strict        as Map
 import           Data.Monoid            ((<>))
 import           Data.Text              (Text)
 import qualified Data.Text              as Text
+import qualified Data.Text.IO           as Text
 
+import           Control.Concurrent     (forkIO)
 import           Control.Concurrent.STM
 import           Control.Lens
-import           Control.Monad.IO.Class (MonadIO)
+import           Control.Monad.IO.Class (MonadIO, liftIO)
 
 import           Text.Megaparsec        (runParserT)
 
@@ -27,126 +29,161 @@ import           Wrappers
 
 main :: IO ()
 main = do
+    (input, output) <- runIrcBot 443 "irc.chat.twitch.tv"
+
+    forkIO (inputLogger input)
+    forkIO (outputLogger input)
+
     ini <- readIniFile "config.ini" >>= either die return
-    conf <- either die return (readConfig ini)
+    botConf <- liftIO (readConfig ini input output)
 
-    runBot conf
+    let botState = initBotState
 
-readConfig :: Ini -> Either String BotConfig
-readConfig ini =
-    BotConfig
-        <$> lookupValue "config" "nick" ini
-        <*> lookupValue "config" "pass" ini
-        <*> lookupValue "config" "channel" ini
+    runBot bot botConf botState
 
-runBot :: MonadIO io => BotConfig -> io ()
-runBot botConf = do
-    botState <- initBotState botConf
+-- log messages sent from server to stdout
+outputlogger :: TChan Text -> IO ()
+outputlogger output = forever $ do
+    msg <- atomically (peekTChan output)
+    Text.putStrLn ("> " <> msg)
 
-    conn <- connectWithTLS' stdoutLogger "irc.chat.twitch.tv" 443 2
-    let conn' = conn { _onconnect = onConnect }
+-- log messages sent to server to stdout
+inputLogger :: TChan Text -> IO ()
+inputLogger input = forever $ do
+    msg <- atomically (peekTChan input)
+    Text.putStrLn ("< " <> msg)
 
-    startStateful conn' ircConf botState
+contents :: Text -> Text
+contents msg = case Text.splitOn ":" of
+    [] -> ""
+    xs -> xs !! (length xs  - 1)
 
-    where
-        ircConf = defaultIRCConf (botConf^.botNick)
-            & password .~ Just (botConf^.pass)
-            & eventHandlers .~ msgHandler : defaultEventHandlers
+bot :: Bot ()
+bot = do
+    pass <- view botPass
+    nick <- view botNick
+    chan <- view channel
 
-onConnect :: Bot ()
-onConnect = do
-    s <- state
+    send ("PASS :" <> pass)
+    send ("NICK :" <> nick)
 
-    let nickname = s^.config.botNick
-    let chan = s^.config.channel
+    send ("JOIN :" <> chan)
 
-    send $ RawMsg ("NICK " <> nickname)
-    send $ RawMsg ("JOIN " <> chan)
+send :: Text -> Bot ()
+send msg = do
+    input <- view inputChan
+    atomically (writeTChan input msg)
 
-    return ()
+-- runBot :: MonadIO io => BotConfig -> io ()
+-- runBot botConf = do
+--     botState <- initBotState botConf
 
-msgHandler :: EventHandler BotState
-msgHandler = EventHandler "bot" EPrivmsg $ \event ->
-    case event^.message of
-        Privmsg _ (Right msg) -> do
-            let user = extractUser event
-            cmd <- runParserT command "" msg
+--     conn <- connectWithTLS' stdoutLogger "irc.chat.twitch.tv" 443 2
+--     let conn' = conn { _onconnect = onConnect }
 
-            mapM_ (handleCommand user) cmd
+--     startStateful conn' ircConf botState
 
-        _ -> return ()
+--     where
+--         ircConf = defaultIRCConf (botConf^.botNick)
+--             & eventHandlers .~ msgHandler : defaultEventHandlers
 
-    where
-        extractUser :: Event Text -> Maybe Text
-        extractUser event =
-            case event^.source of
-                User user      -> Just user
-                Channel _ user -> Just user
-                _              -> Nothing
+-- onConnect :: Bot ()
+-- onConnect = do
+--     s <- state
 
-handleCommand :: Maybe Text -> Command -> Bot ()
-handleCommand user cmd =
-    case cmd of
-        CmdUnknown -> replyTo user "idk that command :/"
+--     let nickname = s^.config.botNick
+--     let chan = s^.config.channel
 
-        CmdHi target -> case target of
-            Just _  -> replyTo target "hi! KonCha"
-            Nothing -> replyTo user "hi! KonCha"
-        CmdBye target -> case target of
-            Just _  -> replyTo target "cya! KonCha"
-            Nothing -> replyTo user "cya! KonCha"
-        CmdCommands -> do
-            s <- state
+--     send $ RawMsg ("PASS " <> s^.config.pass)
+--     send (Nick nickname)
+--     send $ RawMsg "CAP REQ :twitch.tv/tags"
+--     send (Join chan)
 
-            dynCmds <- fmap (map (cons '!')) getDynCommands
-            let commands = map (cons '!') $ Map.keys (s^.staticCmds)
-            let cmdList = (Text.concat . intersperse ", ") (commands <> dynCmds)
+--     return ()
 
-            replyTo user cmdList
+-- msgHandler :: EventHandler BotState
+-- msgHandler = EventHandler "bot" EEverything $ \event -> do
+--     liftIO $ print (event^.raw)
+--     case event^.message of
+--         Privmsg _ (Right msg) -> do
+--             let user = extractUser event
+--             cmd <- runParserT command "" msg
 
-        CmdAdd cmdName cmdText -> do
-            s <- state
+--             mapM_ (handleCommand user) cmd
 
-            cmdAliases <- getAliases
-            dynCmds <- getDynCommands
+--         _ -> return ()
 
-            if Map.member cmdName (s^.staticCmds) || cmdName `elem` cmdAliases || cmdName `elem` dynCmds
-                then
-                    replyTo user ("!" <> cmdName <> " is already the name of an existing command")
-                else do
-                    addDynCommand cmdName cmdText
-                    replyTo user ("Added command !" <> cmdName)
+--     where
+--         extractUser :: Event Text -> Maybe Text
+--         extractUser event =
+--             case event^.source of
+--                 User user      -> Just user
+--                 Channel _ user -> Just user
+--                 _              -> Nothing
 
-        CmdRemove cmdName -> do
-            removeDynCommand cmdName
-            replyTo user ("Removed command !" <> cmdName)
-        CmdDynamic cmdName -> runDynCommand cmdName
+-- handleCommand :: Maybe Text -> Command -> Bot ()
+-- handleCommand user cmd =
+--     case cmd of
+--         CmdUnknown -> replyTo user "idk that command :/"
 
-addDynCommand :: Text -> Text -> Bot ()
-addDynCommand cmdName cmdText = do
-    s <- state
-    atomicallyL $ modifyTVar (s^.dynamicCmds) (Map.insert cmdName cmdText)
+--         CmdHi target -> case target of
+--             Just _  -> replyTo target "hi! KonCha"
+--             Nothing -> replyTo user "hi! KonCha"
+--         CmdBye target -> case target of
+--             Just _  -> replyTo target "cya! KonCha"
+--             Nothing -> replyTo user "cya! KonCha"
+--         CmdCommands -> do
+--             s <- state
 
-removeDynCommand :: Text -> Bot ()
-removeDynCommand cmdName = do
-    s <- state
-    atomicallyL $ modifyTVar (s^.dynamicCmds) (Map.delete cmdName)
+--             dynCmds <- fmap (map (cons '!')) getDynCommands
+--             let commands = map (cons '!') $ Map.keys (s^.staticCmds)
+--             let cmdList = (Text.concat . intersperse ", ") (commands <> dynCmds)
 
-runDynCommand :: Text -> Bot ()
-runDynCommand cmdName = do
-    s <- state
-    cmdMap <- readTVarIOL (s^.dynamicCmds)
+--             replyTo user cmdList
 
-    mapM_ announce (Map.lookup cmdName cmdMap)
+--         CmdAdd cmdName cmdText -> do
+--             s <- state
 
-getDynCommands :: Bot [Text]
-getDynCommands = do
-    s <- state
-    cmdMap <- readTVarIOL (s^.dynamicCmds)
+--             cmdAliases <- getAliases
+--             dynCmds <- getDynCommands
 
-    return (Map.keys cmdMap)
+--             if Map.member cmdName (s^.staticCmds) || cmdName `elem` cmdAliases || cmdName `elem` dynCmds
+--                 then
+--                     replyTo user ("!" <> cmdName <> " is already the name of an existing command")
+--                 else do
+--                     addDynCommand cmdName cmdText
+--                     replyTo user ("Added command !" <> cmdName)
 
-getAliases :: Bot [Text]
-getAliases = do
-    s <- state
-    return (concat $ (s^.staticCmds) ^.. traverse . aliases)
+--         CmdRemove cmdName -> do
+--             removeDynCommand cmdName
+--             replyTo user ("Removed command !" <> cmdName)
+--         CmdDynamic cmdName -> runDynCommand cmdName
+
+-- addDynCommand :: Text -> Text -> Bot ()
+-- addDynCommand cmdName cmdText = do
+--     s <- state
+--     atomicallyL $ modifyTVar (s^.dynamicCmds) (Map.insert cmdName cmdText)
+
+-- removeDynCommand :: Text -> Bot ()
+-- removeDynCommand cmdName = do
+--     s <- state
+--     atomicallyL $ modifyTVar (s^.dynamicCmds) (Map.delete cmdName)
+
+-- runDynCommand :: Text -> Bot ()
+-- runDynCommand cmdName = do
+--     s <- state
+--     cmdMap <- readTVarIOL (s^.dynamicCmds)
+
+--     mapM_ announce (Map.lookup cmdName cmdMap)
+
+-- getDynCommands :: Bot [Text]
+-- getDynCommands = do
+--     s <- state
+--     cmdMap <- readTVarIOL (s^.dynamicCmds)
+
+--     return (Map.keys cmdMap)
+
+-- getAliases :: Bot [Text]
+-- getAliases = do
+--     s <- state
+--     return (concat $ (s^.staticCmds) ^.. traverse . aliases)
