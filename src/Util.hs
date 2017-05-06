486c122ff57ed65819df0f481bf9044504dd4325
frublox <lainislaw@gmail.com>
2017-05-05 23:54:39 -0400

    Fix textContains function

--- src/Util.hs
+++ src/Util.hs
@@ -1,6 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Util where
 
+import           Data.Text              (Text)
+import qualified Data.Text              as Text
+
 import           Control.Concurrent.STM
+
 import           Text.Megaparsec
 
 readAllTChan :: TChan a -> STM [a]
@@ -16,3 +22,8 @@ runParserTMaybe :: Monad m => ParsecT e s m a -> s -> m (Maybe a)
 runParserTMaybe p s = do
     result <- runParserT p "" s
     return $ either (const Nothing) Just result
+
+textContains :: Text -> Text -> Bool
+textContains y x = case Text.breakOn x y of
+    (_, "") -> False
+    _       -> True
