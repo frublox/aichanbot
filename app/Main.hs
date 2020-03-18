544067e52b1788d5effa424c333cc8f7e89bfd75
frublox <lainislaw@gmail.com>
2020-03-18 18:35:54 -0400

    Format Main

--- app/Main.hs
+++ app/Main.hs
@@ -2,21 +2,25 @@
 
 module Main where
 
-import           Control.Applicative (liftA2)
+import           Control.Applicative            ( liftA2 )
 import           Control.Lens
-import           Control.Monad       (forM_, when)
+import           Control.Monad                  ( forM_
+                                                , when
+                                                )
 import           Data.Aeson.Lens
-import           Data.ByteString     (ByteString)
-import qualified Data.HashMap.Strict as HashMap
-import           Data.Text           (Text)
+import           Data.ByteString                ( ByteString )
+import qualified Data.HashMap.Strict           as HashMap
+import           Data.Text                      ( Text )
 
-import           Bot.Conduit         (botC)
-import qualified Bot.Env             as Env
-import           Bot.EventHandlers   (msgHandler, pingHandler)
-import           Bot.Init            (onConnect)
-import           Bot.Type            (runBot)
+import           Bot.Conduit                    ( botC )
+import qualified Bot.Env                       as Env
+import           Bot.EventHandlers              ( msgHandler
+                                                , pingHandler
+                                                )
+import           Bot.Init                       ( onConnect )
+import           Bot.Type                       ( runBot )
 import qualified Irc
-import           Util                (textContains)
+import           Util                           ( textContains )
 
 port :: Int
 port = 6667
